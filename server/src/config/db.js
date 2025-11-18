f12d8166d018133a5abfc0ceff308c08912dee43
KomfortKimko <komfortkimk@gmail.com>
2025-11-18 08:44:31 +0300

    changed the mongodb url to the atlas one instead of the local one

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || process.env.MONGODBATLAS_URI);
+    await mongoose.connect(process.env.MONGODBATLAS_URI || process.env.MONGODB_URI);
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
